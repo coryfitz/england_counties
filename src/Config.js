3e21a74ef4fe9a87834e7df7631add503fe03c2c
Cory Fitz <coryalanfitz@gmail.com>
2023-04-21 22:03:36 +0100

    initial commit

---
+++ src/Config.js
@@ -0,0 +1,9 @@
+export const title = 'Counties of England';
+
+export const unitType = 'county';
+export const unitNamesPath = './data/countyNames.json';
+export const geoUrl ='/counties.json';
+
+export const scale = 3100;
+export const center_left = -3;
+export const center_right = 125;
