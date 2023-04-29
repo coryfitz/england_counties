e3b72fe933f0aec0a1d9762a5ab5c7ec35c1c4a2
Cory Fitz <coryalanfitz@gmail.com>
2023-04-29 11:08:25 +0100

    responsive buttons

--- src/Home.js
+++ src/Home.js
@@ -7,8 +7,10 @@ function Home() {
 
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <a className="navbar-brand justify-content-end" href="/" style={{color: 'black', marginLeft: 15}}><h2>{title}</h2></a>
-                <a type="button" className="btn btn-success" href="study" style={{margin: 5}}>Study</a>
-                <a type="button" className="btn btn-success" href="quiz" style={{margin: 5}}>Quiz</a>
+                <div>
+                    <a type="button" className="btn btn-success" href="study" style={{margin: 5}}>Study</a>
+                    <a type="button" className="btn btn-success" href="quiz" style={{margin: 5}}>Quiz</a>
+                </div>
             </nav>
        
             <div className="container" style={{marginTop: 80, textAlign: 'center'}}>
