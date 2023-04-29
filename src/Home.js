addd90f35b96409d353ed591d02cf70f93d0411b
Cory Fitz <coryalanfitz@gmail.com>
2023-04-29 11:00:34 +0100

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -11,7 +11,7 @@ function Home() {
                 <a type="button" className="btn btn-success" href="quiz" style={{margin: 5}}>Quiz</a>
             </nav>
        
-            <div className="container" style={{position: 'fixed', marginTop: 80, textAlign: 'center'}}>
+            <div className="container" style={{marginTop: 80, textAlign: 'center'}}>
                 <h2>
                     Select study or quiz mode
                 </h2>
