ae82160a97def4602aed03ea49694368609a8c42
Cory Fitz <coryalanfitz@gmail.com>
2023-04-29 10:57:39 +0100

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import {title} from './Config';
 
 function Home() {
     return (
-        <div>
+        <div className="container">
 
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <a className="navbar-brand justify-content-end" href="/" style={{color: 'black', marginLeft: 15}}><h2>{title}</h2></a>
