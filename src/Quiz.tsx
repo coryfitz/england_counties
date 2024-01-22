26cebebe457e126f00a06670b72152602347515f
Cory Fitz <coryalanfitz@gmail.com>
2024-01-22 23:50:24 +0000

    converted Study from JS to TS

--- src/Quiz.tsx
+++ src/Quiz.tsx
@@ -2,119 +2,15 @@ import React, { useEffect, useState } from "react";
 import { ComposableMap, Geographies, Geography } from "react-simple-maps";
 import _ from 'lodash';
 import {scale, center_left, center_right, title, unitType, geoUrl} from './Config.tsx';
-
-//const unitNamesJson = import('./data/provinceNames.json');
 import unitNamesJson from './data/englandCountyNames.json';
 const allUnits = unitNamesJson;
 
-function Map() {
-  const [quiz, setQuiz] = useState(getNewShuffledQuiz());
-  const [quizIndex, setQuizIndex] = useState(0);
-  const [quizEnded, setQuizEnded] = useState(false);
-  const [done, setDone] = useState([]);
-  const [message, setMessage] = useState('Please select:');
-
-  const handleUnitClick = (unitGeo) => {
-
-    if (quizEnded) return;
-
-    const unitName = unitGeo?.properties?.[unitType];
-    if (!unitName) return;
-
-    if (unitName === quiz[quizIndex].unitName) {
-        setDone(done => [...done, unitName]);
-        moveToNextUnit(unitName, true, false);
-    }
-}
-
-  function restartQuiz() {
-    setQuiz(getNewShuffledQuiz());
-    setQuizIndex(0);
-    setQuizEnded(false);
-    setDone([]);
-    setMessage('Please select:');
-  }
-
-  const moveToNextUnit = (unitName) => {
-    // update the quiz:
-    setQuiz([...quiz.slice(0, quizIndex), {
-        unitName,
-    },
-    ...quiz.slice(quizIndex + 1)
-    ]);
-
-    // move to the next unit:
-    setQuizIndex(quizIndex + 1);
-
-    // check if the quiz has ended:
-    if (quizIndex + 1 === quiz.length) {
-        setQuizEnded(true);
-    }
-  }
-
-  useEffect(() => {
-    if (done.length === (Object.keys(unitNamesJson)).length) {
-    setMessage('Congratulations on completing the quiz');
-  }
-  }, [done])
-
-  
-  return (
-    <div className="container">
-
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand justify-content-end" href="/" style={{color: 'black', marginLeft: 15}}><h2>{title}</h2></a>
-        <div>
-          <a type="button" className="btn btn-success" href="study" style={{margin: 5}}>Study</a>
-          <a type="button" className="btn btn-success" href="quiz" style={{margin: 5}}>Quiz</a>
-        </div>
-      </nav>
-
-      <div class="row">
-
-        <div class="col-md-4">
-          <TextBox quiz={quiz} quizIndex={quizIndex} message={message}/>
-          <button className="btn btn-success" style={{margin: 10}} onClick={restartQuiz}>Restart</button>
-        </div>
-
-        <div class="col-md-8">
-          <div className="card" style={{marginTop: 40}}>
-            <div className="card-body">
-              <ComposableMap
-                projectionConfig={{
-                  scale: scale,
-                  center: [center_left, center_right]
-                }}>
-                <Geographies geography={geoUrl}>
-                  {({ geographies }) => 
-                  geographies.map(geo => (
-                    <Unit 
-                      key={geo?.rsmKey} 
-                      geography={geo}
-                      currQuizUnit={((quizIndex < quiz.length) && quiz[quizIndex].unitName) || ''}
-                      handleUnitClick={handleUnitClick}
-                      done={done}
-                      unitType={unitType}
-                    />
-                  ))}
-                </Geographies>
-              </ComposableMap>
-            </div>
-          </div>
-        </div>
-
-      </div>
-
-    </div>
-  )
-}
-
 function getNewShuffledQuiz() {
-  return _.map(
-      _.shuffle(Object.keys(unitNamesJson)), unitName => ({
-          unitName,
-      })
-  );
+  return _.map(_.shuffle(Object.keys(unitNamesJson)), function(unitName) {
+    return {
+      unitName,
+    };
+  });
 }
 
 function TextBox ({quiz, quizIndex, message}) {
@@ -144,44 +40,149 @@ function TextBox ({quiz, quizIndex, message}) {
   )
 }
 
-function Unit({geography, currQuizUnit, handleUnitClick, done, unitType}) {
+function Unit({geography, currQuizUnit, handleUnitClick, done, unitType, quiz, quizIndex, quizEnded, setDone, moveToNextUnit, setQuiz, setQuizIndex, setQuizEnded}) {
   const unitName = geography?.properties?.[unitType];
   const currUnit = allUnits[unitName];
   if (!currUnit) { return (<></>); }
   let unitDone = false;
 
   if (done.includes(unitName)) {
-    unitDone= true;
+    unitDone = true;
   };
 
   return (
     <Geography 
-    geography={geography}
-    style={{
-      default: {
-        fill: unitDone ? 'green' : 'grey',
-        outline: 'none',
-      },
-      hover: {
-          fill: '#3C3B6E',
+      geography={geography}
+      style={{
+        default: {
+          fill: unitDone ? 'green' : 'grey',
           outline: 'none',
-      },
-      pressed: {
-        outline: 'none',
-        fill: unitName === currQuizUnit ? 'green' : 'red'
-    }
-    }}
-    onClick={() => handleUnitClick(geography)}
+        },
+        hover: {
+            fill: '#3C3B6E',
+            outline: 'none',
+        },
+        pressed: {
+          outline: 'none',
+          fill: unitName === currQuizUnit ? 'green' : 'red'
+        }
+      }}
+      onClick={() => handleUnitClick(geography, quiz, quizIndex, quizEnded, setDone, moveToNextUnit,  setQuiz, setQuizIndex, setQuizEnded)}
     />
-    )
+  )
+}
+
+function handleUnitClick(unitGeo, quiz, quizIndex, quizEnded, setDone, moveToNextUnit,  setQuiz, setQuizIndex, setQuizEnded) {
+  if (quizEnded) return;
+
+  const unitName = unitGeo?.properties?.[unitType];
+  if (!unitName) return;
+
+  if (unitName === quiz[quizIndex].unitName) {
+    setDone(function(done) {
+      return [...done, unitName];
+  });
+      moveToNextUnit(unitName, quiz, setQuiz, setQuizIndex, quizIndex, setQuizEnded);
+  }
+}
+
+function renderGeographies(geographies, done, quiz, quizIndex, quizEnded, setDone, moveToNextUnit, setQuiz, setQuizIndex, setQuizEnded) {
+  return geographies.map(function(geo) {
+    return (
+      <Unit 
+        key={geo?.rsmKey} 
+        geography={geo}
+        currQuizUnit={((quizIndex < quiz.length) && quiz[quizIndex].unitName) || ''}
+        handleUnitClick={handleUnitClick}
+        done={done}
+        unitType={unitType}
+        quiz={quiz}
+        quizIndex={quizIndex}
+        quizEnded={quizEnded}
+        setDone={setDone}
+        moveToNextUnit={moveToNextUnit}
+        setQuiz={setQuiz}
+        setQuizIndex={setQuizIndex}
+        setQuizEnded={setQuizEnded}
+      />
+    );
+  });
+}
+
+function restartQuiz(setQuiz, setQuizIndex, setQuizEnded, setDone, setMessage) {
+  setQuiz(getNewShuffledQuiz());
+  setQuizIndex(0);
+  setQuizEnded(false);
+  setDone([]);
+  setMessage('Please select:');
+}
+
+function moveToNextUnit(unitName, quiz, setQuiz, setQuizIndex, quizIndex, setQuizEnded) {
+  // update the quiz:
+  setQuiz([...quiz.slice(0, quizIndex), {
+      unitName,
+  },
+  ...quiz.slice(quizIndex + 1)
+  ]);
+
+  // move to the next unit:
+  setQuizIndex(quizIndex + 1);
+
+  // check if the quiz has ended:
+  if (quizIndex + 1 === quiz.length) {
+      setQuizEnded(true);
+  }
+}
+
+function effectFunction(done, setMessage) {
+  if (done.length === Object.keys(unitNamesJson).length) {
+    setMessage('Congratulations on completing the quiz');
+  }
 }
 
 function Quiz() {
-    return (
+  const [quiz, setQuiz] = useState(getNewShuffledQuiz());
+  const [quizIndex, setQuizIndex] = useState(0);
+  const [quizEnded, setQuizEnded] = useState(false);
+  const [done, setDone] = useState([]);
+  const [message, setMessage] = useState('Please select:');
+
+  useEffect(() => {effectFunction(done, setMessage)}, [done]);
+
+  return (
+    <div className="container">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand justify-content-end" href="/" style={{color: 'black', marginLeft: 15}}><h2>{title}</h2></a>
         <div>
-          <Map />
+          <a type="button" className="btn btn-success" href="study" style={{margin: 5}}>Study</a>
+          <a type="button" className="btn btn-success" href="quiz" style={{margin: 5}}>Quiz</a>
         </div>
-    )
+      </nav>
+      <div className="row">
+        <div className="col-md-4">
+          <TextBox quiz={quiz} quizIndex={quizIndex} message={message}/>
+          <button className="btn btn-success" style={{margin: 10}} onClick={() => restartQuiz(setQuiz, setQuizIndex, setQuizEnded, setDone, setMessage)}>Restart</button>
+        </div>
+        <div className="col-md-8">
+          <div className="card" style={{marginTop: 40}}>
+            <div className="card-body">
+              <ComposableMap
+                projectionConfig={{
+                  scale: scale,
+                  center: [center_left, center_right]
+                }}>
+                <Geographies geography={geoUrl}>
+                {function(geographyProps) {
+                  return renderGeographies(geographyProps.geographies, done, quiz, quizIndex, quizEnded, setDone, moveToNextUnit,  setQuiz, setQuizIndex, setQuizEnded);
+                }}
+              </Geographies>
+              </ComposableMap>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Quiz;
--- src/Study.tsx
+++ src/Study.tsx
@@ -1,69 +1,26 @@
 import React, { useState } from "react";
-import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+import { ComposableMap, Geographies, Geography, GeographyProps } from "react-simple-maps";
 import {scale, center_left, center_right, title, unitType, geoUrl} from './Config.tsx';
-import unitNamesJson from './data/englandCountyNames.json'
-const allUnits = unitNamesJson;
 
-function Map() {
-  const [selectedUnit, setSelectedUnit] = useState(null);
-  const message = useState(`Please select any ${unitType} to reveal its name`);
+const unitNamesJson = require('./data/englandCountyNames.json');
 
-  function handleUnitClick(unitGeo) {
-    const unitName = unitGeo?.properties?.[unitType];
-    setSelectedUnit(unitName);
-  }
-
-  function handleMouseLeave() {
-    setSelectedUnit(null);
-  }
-
-  
-  return (
-    <div className="container">
-      
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand justify-content-end" href="/" style={{color: 'black', marginLeft: 15}}><h2>{title}</h2></a>
-        <div>
-          <a type="button" className="btn btn-success" href="study" style={{margin: 5}}>Study</a>
-          <a type="button" className="btn btn-success" href="quiz" style={{margin: 5}}>Quiz</a>
-        </div>
-      </nav>
-
-      <div class="row">
-        <div class="col-md-4">
-          <TextBox selectedUnit={selectedUnit} message={message}/>
-        </div>
-        <div class="col-md-8">
-          <div className="card" style={{ marginTop: 40}}>
-              <div className="card-body">
-                <ComposableMap
-                  projectionConfig={{
-                    scale: scale,
-                    center: [center_left, center_right]
-                  }}>
-                  <Geographies geography={geoUrl}>
-                    {({ geographies }) => 
-                    geographies.map(geo => (
-                      <Unit 
-                        key={geo?.rsmKey} 
-                        geography={geo}
-                        handleUnitClick={handleUnitClick}
-                        handleMouseLeave={handleMouseLeave}
-                        unitType={unitType}
-                      />
-                    ))}
-                  </Geographies>
-                </ComposableMap>
-              </div>
-          </div>
-        </div>
-      </div>
-
-    </div>
-  )
+interface UnitDetails {
+  val: number;
 }
 
-function TextBox ({selectedUnit, message}) {
+interface UnitNames {
+  [key: string]: UnitDetails;
+}
+
+const allUnits: UnitNames = unitNamesJson;
+
+interface TextBoxProps {
+  selectedUnit: string | null;
+  message: string;
+}
+
+function TextBox(props: TextBoxProps) {
+  const { selectedUnit, message } = props;
   const unitTypeUpper = unitType[0].toUpperCase() + unitType.slice(1);
   return (
     <div>
@@ -81,8 +38,17 @@ function TextBox ({selectedUnit, message}) {
   )
 }
 
-function Unit({geography, handleUnitClick, handleMouseLeave, unitType}) {
-  const unitName = geography?.properties?.[unitType]
+interface UnitProps {
+  geography: GeographyProps["geography"];
+  handleUnitClick: (unitGeo: GeographyProps["geography"], setSelectedUnit: React.Dispatch<React.SetStateAction<string | null>>) => void;
+  handleMouseLeave: (setSelectedUnit: React.Dispatch<React.SetStateAction<string | null>>) => void;
+  unitType: string;
+  setSelectedUnit: React.Dispatch<React.SetStateAction<string | null>>;
+}
+
+function Unit(props: UnitProps) {
+  const { geography, handleUnitClick, handleMouseLeave, setSelectedUnit } = props
+  const unitName = geography?.properties?.[unitType] as string
   const currUnit = allUnits[unitName];
   if (!currUnit) { return (<></>); }
 
@@ -102,18 +68,73 @@ function Unit({geography, handleUnitClick, handleMouseLeave, unitType}) {
         outline: 'none',
     }
     }}
-    onMouseEnter={() => handleUnitClick(geography)}
-    onMouseLeave={() => handleMouseLeave(geography)}
+    onMouseEnter={() => handleUnitClick(geography, setSelectedUnit)}
+    onMouseLeave={() => handleMouseLeave(setSelectedUnit)}
     />
     )
 }
 
-function Study() {
+function handleUnitClick(unitGeo: GeographyProps["geography"], setSelectedUnit: React.Dispatch<React.SetStateAction<string | null>>) {
+  const unitName = unitGeo?.properties?.[unitType];
+  setSelectedUnit(unitName);
+}
+
+function handleMouseLeave(setSelectedUnit: React.Dispatch<React.SetStateAction<string | null>>) {
+  setSelectedUnit(null);
+}
+
+function renderGeographies(geographies: GeographyProps["geography"][], setSelectedUnit: React.Dispatch<React.SetStateAction<string | null>>) {
+  return geographies.map(function(geo) {
     return (
+      <Unit 
+        key={geo?.rsmKey} 
+        geography={geo}
+        handleUnitClick={handleUnitClick}
+        handleMouseLeave={handleMouseLeave}
+        unitType={unitType}
+        setSelectedUnit={setSelectedUnit}
+      />
+    );
+  });
+}
+
+function Study() {
+  const [selectedUnit, setSelectedUnit] = useState<string | null>(null);
+  const message = `Please select any ${unitType} to reveal its name`;
+
+  return (
+    <div className="container">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand justify-content-end" href="/" style={{color: 'black', marginLeft: 15}}><h2>{title}</h2></a>
         <div>
-          <Map />
+          <a type="button" className="btn btn-success" href="study" style={{margin: 5}}>Study</a>
+          <a type="button" className="btn btn-success" href="quiz" style={{margin: 5}}>Quiz</a>
         </div>
-    )
+      </nav>
+      <div className="row">
+        <div className="col-md-4">
+          <TextBox selectedUnit={selectedUnit} message={message}/>
+        </div>
+        <div className="col-md-8">
+          <div className="card" style={{ marginTop: 40}}>
+              <div className="card-body">
+                <ComposableMap
+                  projectionConfig={{
+                    scale: scale,
+                    center: [center_left, center_right]
+                  }}>
+                  <Geographies geography={geoUrl}>
+                    {function(geographyProps) {
+                      return renderGeographies(geographyProps.geographies, setSelectedUnit);
+                    }}
+                  </Geographies>
+                </ComposableMap>
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Study;
